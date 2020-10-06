a6a21fb29c862f3eaf269c452d3ad9d000266a1f
Nick Timms <nicktimms1@hotmail.com>
2020-10-05 18:53:42 -0600

    Config complete for Proxy

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('../dist/index.html'));
+app.use(express.static('../dist'));
 
 app.listen(PORT, () => {
     console.log(`Running server on port: ${PORT}`);
